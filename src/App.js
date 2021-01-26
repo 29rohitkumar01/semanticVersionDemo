c38bbaa396703a5002297bb2a9517d304c50e215
rohit kumar <rohit.kumar@innoplexus.com>
2021-01-26 14:08:44 +0530

    feat: first release

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Semantic versioning release in react
         </a>
       </header>
     </div>
