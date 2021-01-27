95d599d56f560bb2217177913d2f09cb70151a16
rohit kumar <rohit.kumar@innoplexus.com>
2021-01-27 10:27:16 +0530

    BREAKING CHANGE: refactor to use JavaScript features not

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
            <li>test: Adding missing or correcting existing tests</li>
            <li>chore: Changes to the build process or auxiliary tools and libraries such as documentation generation</li>
          </ul>
+         <p>Trying breaking change</p>
       </header>
     </div>
   );
